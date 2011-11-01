73fc29e8c3cefca1895cc85dbafe86c962cb00c3
Nikita Afanasenko <nikita@afanasenko.name>
2011-11-01 11:29:39 +0400

    use different colors for ssh and local term

--- themes/nikitug/nikitug.theme.bash
+++ themes/nikitug/nikitug.theme.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 SCM_THEME_PROMPT_DIRTY=" ${red}✗"
 SCM_THEME_PROMPT_CLEAN=" ${bold_green}✓"
 SCM_THEME_PROMPT_PREFIX="${yellow}"
@@ -14,18 +15,27 @@ RVM_THEME_PROMPT_SUFFIX=" "
 
 function prompt_command()
 {
-    if [ "$(whoami)" = root ]
-    then
-      user_color=$red
-      user_prompt_char="#"
-    else
-      user_color=$white
-      user_prompt_char="→"
-    fi
+  if [ "$(whoami)" = root ]
+  then
+    user_prompt_char_color=$red
+    user_prompt_char="#"
+  else
+    user_prompt_char_color=$white
+    user_prompt_char="→"
+  fi
 
-    prompt_underline="${black}${underline_black}                                       ${reset_color}${normal}"
+  if [ -z "$SSH_TTY" ]
+  then
+    # no ssh connection, assuming local shell
+    location_color=$blue
+  else
+    # connected by ssh
+    location_color=$purple
+  fi
 
-    PS1="${prompt_underline}\n$(date "+%Y-%m-%d") \t ${purple}\u@\h ${reset_color}in ${background_black}\w${normal}\n${yellow}$(scm_prompt_info)${reset_color}${user_color}${user_prompt_char}${normal}${reset_color} "
+  prompt_underline="${black}${underline_black}                                       ${reset_color}${normal}"
+
+  PS1="${prompt_underline}\n$(date "+%Y-%m-%d") \t ${location_color}\u@\h ${reset_color}in ${background_black}\w${normal}\n${yellow}$(scm_prompt_info)${reset_color}${user_prompt_char_color}${user_prompt_char}${normal}${reset_color} "
 }
 
 PROMPT_COMMAND=prompt_command;
